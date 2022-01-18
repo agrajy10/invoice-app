1922d5215ecd7278b19e86aaffac13bebe10d1fd
Agraj Yadav <agrajy10@gmail.com>
2022-01-18 22:00:12 +0530

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './css/main.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Invoice App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      'custom-light': '#F8F8FB',
+      'custom-dark': '#141625',
+      'custom-purple-1': '#DFE3FA',
+      'custom-purple-2': '#7E88C3',
+      'custom-purple-3': '#9277FF',
+      'custom-purple-4': '#7C5DFA',
+      'custom-gray-1': '#888EB0',
+      'custom-gray-2': '#252945',
+      'custom-gray-3': '#1E2139',
+      'custom-gray-4': '#0C0E16',
+      'custom-red-1': '#9277FF',
+      'custom-red-2': '#EC5757'
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Spartan', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
+  plugins: []
+};
